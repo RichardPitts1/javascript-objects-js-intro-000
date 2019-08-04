a1810c25f3dd8c1da5564bd4040169a805e52b2e
Richard Pitts <richardepitts@gmail.com>
2019-08-04 12:44:14 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,25 @@
+var playlist = { song1: "title1",
+                 song2: "title2"
+  
+};
+var aName = "Jimmy Hendrix"
+var aName2 = "Michael Jackson"
+var aTitle = "Purple Haze"
+var aTitle2 = "Beat it"
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+console.log(updatePlaylist(playlist, aName, aTitle));
+
+function removeFromPlaylist(playlist, artistName, songTitle) {
+  delete playlist(artistName, songTitle);
+  return playlist;
+}
+
+console.log(removeFromPlaylist(playlist, aName, aTitle));
+
+
+
